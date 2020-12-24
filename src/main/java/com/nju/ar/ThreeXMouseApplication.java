c83c834602f8e3c2a5b2709ec1aba0e1905d54ef
oGsLP <1145234011@qq.com>
2020-12-24 15:40:49 +0800

    Initial Commmit

---
+++ src/main/java/com/nju/ar/ThreeXMouseApplication.java
@@ -0,0 +1,13 @@
+package com.nju.ar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreeXMouseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThreeXMouseApplication.class, args);
+    }
+
+}
